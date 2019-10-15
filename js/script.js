1639a04a1e85837d39387491c783c462da05b9b3
varun satvara <varunsatvara1997@gmail.com>
2019-10-15 17:03:44 +0530

    update

--- js/script.js
+++ js/script.js
@@ -18,3 +18,21 @@ document.getElementById("yellow").onclick = function() {
     document.getElementById("output").style.color = '#f1c40f';
     document.getElementById("output").style.backgroundColor = "#ff3838";
 }
+
+function add() {
+    var outputElement = document.getElementById("output");
+    var computedFontSize = Number(window.getComputedStyle(document.getElementById("output")).getPropertyValue('font-size').match(/\d+/)[0]);
+
+    computedFontSize = computedFontSize + 1;
+
+    outputElement.style.fontSize = computedFontSize + "px";
+}
+
+function minus() {
+    var outputElement = document.getElementById("output");
+    var computedFontSize = Number(window.getComputedStyle(document.getElementById("output")).getPropertyValue('font-size').match(/\d+/)[0]);
+
+    computedFontSize = computedFontSize - 1;
+
+    outputElement.style.fontSize = computedFontSize + "px";
+}
