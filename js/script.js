b75650c58da05dcbd26aeffda50ef982222f7367
varun satvara <varunsatvara1997@gmail.com>
2019-10-15 15:11:31 +0530

    first cmt

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("red").onclick = function() {
+    document.getElementById("output").style.color = '#f53b57';
+    document.getElementById("output").style.backgroundColor = "#fffa65";
+}
+document.getElementById("grass").onclick = function() {
+    document.getElementById("output").style.color = '#009432';
+    document.getElementById("output").style.backgroundColor = "#f1c40f";
+}
+document.getElementById("blue").onclick = function() {
+    document.getElementById("output").style.color = '#1B1464';
+    document.getElementById("output").style.backgroundColor = "#ecf0f1";
+}
+document.getElementById("md-blue").onclick = function() {
+    document.getElementById("output").style.color = '#2c3e50';
+    document.getElementById("output").style.backgroundColor = "#7ed6df";
+}
+document.getElementById("yellow").onclick = function() {
+    document.getElementById("output").style.color = '#f1c40f';
+    document.getElementById("output").style.backgroundColor = "#ff3838";
+}
